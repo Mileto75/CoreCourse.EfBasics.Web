7ce65bfbd9dbf1567d3e52658fe551027c715bc4
Mileto Di Marco <mileto.di.marco@howest.be>
2022-04-19 08:59:00 +0200

    Add CoursesAddViewmodel

--- CoreCourse.EfBasics.Web/Controllers/CoursesController.cs
+++ CoreCourse.EfBasics.Web/Controllers/CoursesController.cs
@@ -1,7 +1,9 @@
-﻿using CoreCourse.EfBasics.Web.Data;
+﻿using CoreCourse.EfBasics.Core.Entities;
+using CoreCourse.EfBasics.Web.Data;
 using CoreCourse.EfBasics.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +18,7 @@ namespace CoreCourse.EfBasics.Web.Controllers
             //constructor injection
             _schoolDbContext = schoolDbContext;
         }
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             //get the data
@@ -30,11 +33,15 @@ namespace CoreCourse.EfBasics.Web.Controllers
             return View(coursesIndexViewModel);
         }
 
+        [HttpGet]
         public async Task<IActionResult> Detail(int id)
         {
             //get the course
             var course = await _schoolDbContext
-                .Courses.FirstOrDefaultAsync
+                .Courses
+                .Include(c => c.Teacher)
+                .Include(c => c.Students)
+                .FirstOrDefaultAsync
                 (c => c.Id == id);
             //fill the model
             var coursesDetailViewModel
@@ -48,6 +55,34 @@ namespace CoreCourse.EfBasics.Web.Controllers
                 };
             //send to the view
             return View(coursesDetailViewModel);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            
+            //follows oop principles
+            //add a new course
+            Course newCourse = new Course();
+            newCourse.Name = "Basic It skills";
+            newCourse.TeacherId = 2;
+            //get the students
+            var students = await _schoolDbContext.Students.ToListAsync();
+            newCourse.Students = students;
+            //add dbcontext
+            await _schoolDbContext.Courses.AddAsync(newCourse);
+            //save to the database
+            try {
+                await _schoolDbContext.SaveChangesAsync();
+            }
+            catch(DbUpdateException ex)
+            {
+                //log the exception
+                Console.WriteLine(ex.Message);
+                return View("Error");
+            }
+
+            
+            return Content("Added");
         }
     }
 }
---
+++ CoreCourse.EfBasics.Web/ViewModels/CoursesAddViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreCourse.EfBasics.Web.ViewModels
+{
+    public class CoursesAddViewModel
+    {
+        [Required(ErrorMessage = "Please provide a title!")]
+        [Display(Name = "Course title")]
+        public string Title { get; set; }
+        [Display(Name = "Teacher")]
+        //One to Many relation course => Teacher
+        public IEnumerable<SelectListItem> Teachers { get; set; }
+        public int TeacherId { get; set; }
+    }
+}
